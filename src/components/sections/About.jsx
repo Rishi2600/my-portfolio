39970c1a49ee3ca0b4e89fdb33f67001ebd70275
Rishi2600 <rishiraj6177@gmail.com>
2025-11-13 22:43:06 +0530

    about

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+    reutrn(
+        <>
+            <section>
+                <h2>About</h2>
+                <p></p>
+                <p>I build clean, performant web applications. 
+        Currently focused on React and modern frontend technologies.</p>
+            </section>
+        </>
+    )
+}
+
+export default About
