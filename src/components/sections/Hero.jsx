b976899112a5ba5150ab49a06ada3cae9b8b1662
Rishi2600 <rishiraj6177@gmail.com>
2025-11-13 22:41:25 +0530

    hero

---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+    return (
+        <>
+            <section>
+                <h1>Rishi</h1>
+                <p>Full-stack dev</p>
+            </section>
+        </>
+    )
+}
+
+export default Hero
