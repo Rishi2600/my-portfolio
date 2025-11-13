5972687d3f78945dca13747761ddf7e70336ee99
Rishi2600 <rishiraj6177@gmail.com>
2025-11-13 23:00:19 +0530

    stitched basic version

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import Header from "./components/layout/Header"
+import Hero from "./components/sections/Hero"
+import About from "./components/sections/About"
+import Projects from "./components/sections/Projects"
+import ContactUs from "./components/sections/ContactUs"
+import Footer from "./components/layout/Footer"
+
 function App() {
 
   return (
     <>
-      
+      <div>
+        <Header />
+        <Hero />
+        <About />
+        <Projects />
+        <ContactUs />
+        <Footer />
+      </div>
     </>
   )
 }
